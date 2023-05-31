5963fe7ab95a8aa810afcf7acf0443084b08d72c
ThallapureddyPujitha <134999531+ThallapureddyPujitha@users.noreply.github.com>
2023-05-31 15:06:23 +0530

    commit

---
+++ Student1.java
@@ -0,0 +1,25 @@
+package day3;
+
+public class Student1 {
+	//sample method
+	public void name() {
+    System.out.println("hello students");
+	}
+	public void age() {
+	    System.out.println(" students age is 19");
+		}
+	public void phno() {
+	    System.out.println(" students phone no is 9110775518");
+		}
+	//main method
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//creation of object
+		Student1 std=new Student1();
+		std.name();
+		std.age();
+		std.phno();
+
+	}
+
+}
